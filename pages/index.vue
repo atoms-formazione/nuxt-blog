63ab2b1b7e96ca867da32165c5eba497b6e31cc7
Kejsi <kejsibitri01@gmail.com>
2023-02-07 17:52:30 +0100

    AddTheHero

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>This is the home page</div>
+  <TheHero />
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+</style>
