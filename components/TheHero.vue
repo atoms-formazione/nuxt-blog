faf4782ce73363e2d3ad73ea2d51e8d28692e1e8
Kejsi <kejsibitri01@gmail.com>
2023-02-07 17:50:17 +0100

    TheHero

---
+++ components/TheHero.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div>
+    <div class="relative bg">
+        <img src="~/assets/TheHero.png" class="object-cover absolute mix-blend-overlay" />
+        <div class="p-20  top-20 absolute fixed" >
+        <p class="text-white bg-white/20 rounded-lg w-[100px]">ADVENTURE</p>
+        <h1 class="text-white">Richird Norton photorealistic rendering as real photos</h1>
+        </div>
+    </div>
+    </div>
+</template>
+<script setup>
+</script>
+<style lang="">
+    
+</style>
