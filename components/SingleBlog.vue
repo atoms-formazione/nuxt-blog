5a75ed3ab8924bd7d51e49ef46017bc1dc926bcf
Daniele Ghilardini <ghilardinidaniele@gmail.com>
2023-02-07 11:30:03 +0100

    Initialised SingleBlog

---
+++ components/SingleBlog.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps({
+  image: String,
+  tag: String,
+  date: String,
+  title: String,
+  text: String,
+});
+</script>
+
+<template>
+  <div>
+    <img :src="image" alt="Image" class="rounded-bl-md"/>
+    <p>{{ tag }}</p>
+    <p>{{ date }}</p>
+    <p>{{ title }}</p>
+    <p>{{ text }}</p>
+  </div>
+</template>
