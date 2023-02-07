a6d4f8908275f5fe63780ef3d1b4d3b08515986f
Kejsi <kejsibitri01@gmail.com>
2023-02-07 17:59:41 +0100

    Fix TheHero

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,10 +1,10 @@
 <template lang="">
     <div>
-    <div class="relative bg">
-        <img src="~/assets/TheHero.png" class="object-cover absolute mix-blend-overlay" />
-        <div class="p-20  top-20 absolute fixed" >
-        <p class="text-white bg-white/20 rounded-lg w-[100px]">ADVENTURE</p>
-        <h1 class="text-white">Richird Norton photorealistic rendering as real photos</h1>
+    <div class="relative">
+        <img src="~/assets/TheHero.png" class="object-cover mix-blend-overlay" />
+        <div class="absolute top-5 left-5 md:top-20 md:left-20" >
+            <p class="text-white bg-white/20 rounded-lg w-[100px]">ADVENTURE</p>
+            <h1 class="text-white">Richird Norton photorealistic rendering as real photos</h1>
         </div>
     </div>
     </div>
