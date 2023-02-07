b579972d20c6ad96a12af830ddef778332b9d2f6
Pietro Stivanello <stivanellop@yahoo.com>
2023-02-07 14:37:13 +0100

    cards components

---
+++ components/Atoms/singleCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-10">
+    <div v-if="response" class="grid grid-cols-4 gap-5">
+      <div v-for="photo in response">
+        <div>
+          <img :src="photo.image" :alt="photo.title" />
+        </div>
+        <div>
+          <p>{{ photo.title }}</p>
+          <p>{{ photo.publish_date }}</p>
+          <p>{{ photo.description }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const response = await fetch(
+  `https://63e1285c65b57fe60652c60f.mockapi.io/Getdata`
+)
+  .then((res) => res.json())
+  .catch((error) => error);
+console.log("response", response.Getdata);
+console.log("AAAA", response);
+</script>
+
+<style scoped></style>
---
+++ components/molecules/cardsSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="font-serif font-bold text-4xl text-title-gray">
+      Popular Topics
+    </h1>
+    <AtomsCardsNavBar
+      :links="[
+        'All',
+        'Adventure',
+        'Travel',
+        'Fashion',
+        'Technology',
+        'Branding',
+      ]"
+    >
+    </AtomsCardsNavBar>
+
+    <AtomsSingleCard></AtomsSingleCard>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
