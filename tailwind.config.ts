382c2748437a60a4afc924b501725500f37e3d77
Pietro Stivanello <stivanellop@yahoo.com>
2023-02-07 11:25:27 +0100

    configuration added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,20 @@ import type { Config } from "tailwindcss";
 
 export default <Partial<Config>>{
   theme: {
+    colors: {
+      "title-gray": "#495057",
+      "footer-black": "#212529",
+      "bg-gray": "#343A40",
+      "text-light-gray": "#6C757D",
+      "text-white": "#E5E5E5",
+      "active-gold": "#D4A373",
+      "tag-bg-gray": "rgba(255, 255, 255, 0.15)",
+    },
+    fontFamily: {
+      logo: ["League Spartan", "sans-serif"],
+      serif: ["Lora", "serif"],
+      tag: ["Roboto", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
