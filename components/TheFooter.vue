2b50780d241a2352abf3e0a059e7d774a71af98f
PietroS <stivanellop@yahoo.com>
2023-02-12 17:27:24 +0100

    footer + detail hero

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,3 +1,20 @@
 <template>
-  <footer>The footer</footer>
+  <footer
+    class="absolute bottom-0 p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800"
+  >
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+      >2023 |
+      <a href="https://runo.com/" class="hover:underline"
+        >RUNO Publisher Studio</a
+      >
+    </span>
+    <ul
+      class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+    >
+      <li>
+        <a href="#" class="mr-4 hover:underline md:mr-6 text-bg-gray">Subscribe Now</a>
+      </li>
+    
+    </ul>
+  </footer>
 </template>
