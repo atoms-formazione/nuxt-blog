dd7654b57da4dc69d3e13c3904d8cce96e608002
PietroS <stivanellop@yahoo.com>
2023-02-12 18:17:05 +0100

    footer finished

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,82 @@
 <template>
-  <footer
-    class="absolute bottom-0 bg-bg-gray p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800"
+  <footer class="bg-footer-black text-text-white text-center lg:text-left">
+    <div class="container p-6 "  >
+      <div class="grid lg:grid-cols-4 md:grid-cols-2 mx-[5%] gap-x-6">
+        <div class="mb-6">
+          <h5 class="uppercase font-bold mb-2.5 text-gray-800">Contact the Publisher</h5>
+
+          <ul class="list-none mb-0">
+            <li>
+              <a href="#!" class="text-gray-800">mike@runo.com</a>
+            </li>
+            <li>
+              <a href="#!" class="text-gray-800">+554 898 564 988</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="mb-6">
+          <h5 class="uppercase font-bold mb-2.5 text-gray-800">Explorate</h5>
+
+          <ul class="list-none mb-0">
+            <li>
+              <a href="#!" class="text-gray-800">About</a>
+            </li>
+            <li>
+              <a href="#!" class="text-gray-800">Partners</a>
+            </li>
+            <li>
+              <a href="#!" class="text-gray-800">Job Opportunities</a>
+            </li>
+            <li>
+              <a href="#!" class="text-gray-800">Advertise</a>
+            </li>
+            <li>
+              <a href="#!" class="text-gray-800">Membership</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="mb-6">
+          <h5 class="uppercase font-bold mb-2.5 text-gray-800">Headquarter</h5>
+
+          <ul class="list-none mb-0">
+            <li>
+              <a href="#!" class="text-gray-800">191 Dubidu Street, <br> NY 1001, Sydney <br> Australia </a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="mb-6">
+          <h5 class="uppercase font-bold mb-2.5 text-gray-800">Connections</h5>
+
+          <ul class="list-none mb-0">
+            <li>
+              <a href="#!" class="text-gray-800">social icons</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div
+      class="flex items-center mt-3 text-sm text-text-white dark:text-gray-400 sm:mt-0 justify-between bg-bg-gray p-5 px-[5%]"
+    >
+    <div class="text-sm text-text-white sm:text-center dark:text-gray-400">
+      2023 |
+      <a href="https://runo.com/" class="hover:underline"
+        >RUNO Publisher Studio</a
+      >
+    </div>
+   <div>
+     <a href="#" class="mr-4 hover:underline md:mr-6 text-text-white"
+     >Subscribe Now</a
+     >
+    </div>
+    </div>
+  </footer>
+
+  <!-- <footer
+    class=" bg-bg-gray p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800"
   >
     <span class="text-sm text-text-white sm:text-center dark:text-gray-400"
       >2023 |
@@ -16,5 +92,5 @@
       </li>
     
     </ul>
-  </footer>
+  </footer> -->
 </template>
