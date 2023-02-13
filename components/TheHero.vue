e82dc0d7427c5cdd97310a37687398758cbf60ce
Kejmamr <kejsibitri01@gmail.com>
2023-02-13 21:29:59 +0100

    TheHero

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -2,14 +2,23 @@
     <div>
     <div class="relative">
         <img src="~/assets/TheHero.png" class="object-cover mix-blend-overlay" />
-        <div class="absolute top-5 left-5 md:top-20 md:left-20" >
-            <p class="text-white bg-white/20 rounded-lg w-[100px]">ADVENTURE</p>
-            <h1 class="text-white">Richird Norton photorealistic rendering as real photos</h1>
+        <TheHeader />
+        <div class="absolute top-5 left-5 md:top-80 md:left-20" >
+            <Tag :tag="'ADVENTURE'" class="font-bold"/>
+            <div class="text-white not-italic text-3xl font-bold">Richird Norton photorealistic <br> rendering as real photos</div>
+            <div class="flex">
+                 <p  class="text-white"> 08.08.2021</p>
+                 <hr class="w-[30px] text-white px-2 m-3" />
+                 <p class="text-white line-clamp-2"> {{ response.description }}</p>
+            </div>
         </div>
     </div>
     </div>
 </template>
 <script setup>
+const response = await fetch(
+  "https://63e1285c65b57fe60652c60f.mockapi.io/Getdata/1"
+).then((res) => res.json());
 </script>
 <style lang="">
     
